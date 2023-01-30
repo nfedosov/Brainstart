d47de4c1af7dd3d489b81fa60267535c0dca1611
entropy <96539091+nfedosov@users.noreply.github.com>
2023-01-30 10:52:13 +0300

    Fit kalman parameters, cfir grid search added

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ Created on Fri Jan 20 16:57:57 2023
 @author: Fedosov
 """
 
-from stationar_filter import offline_test
+from stationar_filter import offline_test_cfir, offline_test_kf
 
 if __name__ == "__main__":
-    offline_test()
+    offline_test_kf()
+    #offline_test_cfir()
+    
+    
+    
--- stationar_filter.py
+++ stationar_filter.py
@@ -9,10 +9,31 @@ Created on Thu Jan 19 02:12:48 2023
 
 import numpy as np
 import scipy.signal as sn
+from scipy.optimize import nnls
+from tqdm import trange
 
-#data = np.random.randn(10000, 3)
 
+def cfir_grid_search(signal, fs, band = [8.0,12.0], order_range = [10,300]):
+    
+    
+    gt = band_hilbert(signal, fs, )
+    
+    metrics = np.zeros(len(order_range))
+    for i,order in enumerate(order_range):
+        cfir = CFIRBandEnvelopeDetector(band = band, fs = fs, delay_ms=0, n_taps=order, n_fft=fs, reg_coeff=0)
+        filtered = cfir.apply(signal)
+        
+        envelope = np.abs(filtered)
+        
+        metrics = np.sum(gt*envelope)/(np.linalg.norm(envelope)*np.linalg.norm(gt))
+        
+    return metrics
+        
 
+    
+    
+def fit_envelope():
+    pass
 
 
 
@@ -95,11 +116,230 @@ def band_hilbert(x, fs, band, N=None, axis=-1):
     
    
     
-def corr_latency(self, y):
+def corr_latency(self, filtered, gt, gt_method):
     pass
     
     
     
+def generate_matsuda_params(fs: float, fc: float, A: float):
+    arg = 2.0*np.pi*fc/fs
+    Phi = np.array([[np.cos(arg),-np.sin(arg)],[np.sin(arg),np.cos(arg)]])
+    H = np.array([1.0,0.0]).reshape((1,2))
+    
+    return Phi, H    
+    
+
+
+
+
+def fit_kf_parameters(meas, kf, cf, A, fs, n_iter: int = 2, tol: float = 1e-3):
+
+
+    AMP_EPS = 1e-4
+    
+    prev_freq = cf
+    model_error = np.inf
+    for _ in (pb := trange(n_iter, desc="Fitting KF parameters")):
+        amp, freq, q_s, r_s, x_0, P_0 = em_step(meas, kf, pb)
+        #if freq > 13.0:
+        #    freq = 13.0
+        #if freq <8.0:
+        #    freq = 8.0
+        if freq != 10.0:
+            freq = 10.0
+            
+        #if amp < 0.95:
+        #    amp = 0.95
+        if amp != 0.99:
+            amp = 0.99
+            
+        amp = min(amp, 1 - AMP_EPS)
+    
+
+        #mp = MatsudaParams(amp, freq, fs)
+        Phi, H = generate_matsuda_params(fs, freq, A)
+        
+        kf = whiteKF(Phi = Phi, H = H, Q = q_s, R = r_s)
+        
+        
+        #KF = whiteKF(mp, H = H, q_s, KF.psi, r_s)
+        kf.x = x_0
+        kf.P = P_0
+        model_error = abs(freq - prev_freq)
+        prev_freq = freq
+        #if model_error < tol:
+        #    break
+    else:
+        print(f"Didn't converge after {n_iter} iterations; {model_error=}")
+    return kf
+
+
+def em_step(meas, KF, pb):
+    n = len(meas)
+    Phi, A, Q, R = KF.Phi, KF.H, KF.Q, KF.R
+    assert n, "Measurements must be nonempty"
+
+    y = normalize_measurement_dimensions(meas)
+    x, P = apply_kf(KF, y)
+    nll = compute_kf_nll(y, x, P, KF)
+    x_n, P_n, J = apply_kalman_interval_smoother(KF, x, P)
+    P_nt = estimate_adjacent_states_covariances(Phi, Q, A, R, P, J)
+
+    S = compute_aux_em_matrices(x_n, P_n, P_nt)
+    freq, Amp, q_s, r_s = params_update(S, Phi, n)
+    pb.set_description(
+        f"Fitting KF parameters: nll={nll:.2f},"
+        f"f={freq*1000/2/np.pi:.2f}, A={Amp:.4f}, {q_s:.2f}, {r_s:.2f}"
+    )
+    x_0_new = x_n[0]
+    P_0_new = P_n[0]
+
+    return Amp, freq, q_s, r_s, x_0_new, P_0_new
+
+
+def normalize_measurement_dimensions(meas):
+    # prepend nan for to simplify indexing; 0 index is for x and P prior to the measurements
+    n = len(meas)
+    y = [np.array([[np.nan]])] * (n + 1)
+    for t in range(1, n + 1):
+        y[t] = meas[t - 1, np.newaxis, np.newaxis]
+    return y
+
+
+def apply_kf(KF, y):
+    n = len(y) - 1
+    x = [None] * (n + 1)  # pyright: ignore  # x^t_t
+    P = [None] * (n + 1)  # pyright: ignore  # P^t_t
+    x[0], P[0] = KF.x, KF.P
+    for t in range(1, n + 1):
+        x[t], P[t] = KF.step(y[t])
+    return x, P
+
+
+def apply_kalman_interval_smoother(
+    KF, x, P
+) :
+    n = len(x) - 1
+    x_n = [None] * (n + 1)  # pyright: ignore  # x^n_t
+    P_n= [None] * (n + 1)  # pyright: ignore  # P^n_t
+    x_n[n], P_n[n] = x[n], P[n]
+    J = [None] * (n + 1)  # pyright: ignore
+    for t in range(n, 0, -1):
+        x_n[t - 1], P_n[t - 1], J[t - 1] = smoother_step(KF, x[t - 1], P[t - 1], x_n[t], P_n[t])
+
+    return x_n, P_n, J
+
+
+def smoother_step(KF, x, P, x_n, P_n) :
+    """
+    Make one Kalman Smoother step
+    Parameters
+    ----------
+    x : Vec
+        State estimate after KF update step after the forward pass, i.e.
+        x^{t-1}_{t-1} in eq (6.47) in [1]
+    P : Cov
+        State covariance after KF update step after the forward pass, i.e.
+        P^{t-1}_{t-1} in eq. (6.48) in [1]
+    x_n : Vec
+        Smoothed state estimate for the time instaint following the one being
+        currently processed, i.e. x^{n}_{t} in eq. (6.47) in [1]
+    P_n : Cov
+        Smoothed state covariance for the time instant following the one being
+        currently processed, i.e. P^{n}_{t} in eq. (6.47) in [1]
+    Returns
+    -------
+    x_n : Vec
+        Smoothed state estimate for one timestep back, i.e. x^{n}_{t-1} in eq.
+        (6.47) in [1]
+    P_n : Cov
+        Smoothed state covariance for one timestep back, i.e. P^{n}_{t-1} in eq. (6.48) in [1]
+    J : Mat
+        J_{t-1} in eq. (6.49) in [1]
+    Notes
+    -----
+    Code here follows slightly different notation than in em_step(); e.g. here
+    x_n is a state vector for a single time instant compared to an array of
+    state vectors in em_step().
+    References
+    ----------
+    [1] .. Shumway, Robert H., and David S. Stoffer. 2011. Time Series Analysis
+    and Its Applications. Springer Texts in Statistics. New York, NY: Springer
+    New York. https://doi.org/10.1007/978-1-4419-7865-3.
+    """
+    x_, P_ = KF.predict()#KF.predict(x, P)
+
+    J = np.linalg.solve(P_, KF.Phi @ P).T  # P * Phi^T * P_^{-1}; solve is better than inv
+
+    x_n = x + J @ (x_n - x_)
+    P_n = P + J @ (P_n - P_) @ J.T
+
+    return x_n, P_n, J
+
+
+def estimate_adjacent_states_covariances(
+    Phi, Q, A, R, P, J
+):
+    # estimate P^n_{t-1,t-2}
+    n = len(P) - 1
+    P_ = Phi @ P[n - 1] @ Phi.T + Q  # P^{n-1}_n
+    K = np.linalg.solve(A @ P_ @ A.T + R, A @ P[n]).T  # K_n, eq. (6.23) in [1]
+    P_nt = [None] * (n + 1)  # pyright: ignore  # P^n_{t-1, t-2}
+    P_nt[n - 1] = (np.eye(K.shape[0]) - K @ A) @ Phi @ P[n - 1]  # P^n_{n, n-1}, eq.(6.55) in [1]
+
+    for t in range(n, 1, -1):
+        P_nt[t - 2] = (
+            P[t - 1] @ J[t - 2].T + J[t - 1] @ (P_nt[t - 1] - Phi @ P[t - 1]) @ J[t - 2].T
+        )
+    return P_nt
+
+
+def compute_aux_em_matrices(x_n, P_n, P_nt):
+    n = len(x_n) - 1
+    S = {
+        "11": np.zeros_like(P_n[0], dtype=np.longdouble),
+        "10": np.zeros_like(P_nt[0], dtype=np.longdouble),
+        "00": np.zeros_like(P_n[0], dtype=np.longdouble),
+    }
+    for t in range(1, n + 1):
+        S["11"] += x_n[t] @ x_n[t].T + P_n[t]
+        S["10"] += x_n[t] @ x_n[t - 1].T + P_nt[t - 1]
+        S["00"] += x_n[t - 1] @ x_n[t - 1].T + P_n[t - 1]
+    return S
+
+
+def params_update(S, Phi, n) :
+    A = S["00"][0, 0] + S["00"][1, 1]
+    B = S["10"][0, 0] + S["10"][1, 1]
+    C = S["10"][1, 0] - S["10"][0, 1]
+    D = S["11"][0, 0] + S["11"][1, 1]
+    f = max(C / B, 0)
+    Amp = np.sqrt(B**2 + C**2) / A
+    q_s = np.sqrt(max(0.5 * (D - Amp**2 * A) / n, 1e-6))
+    r_s = np.sqrt(
+        (S["11"][0, 0])# - 2 * S["10"][2, :] @ Phi.T[:, 2] + (Phi[2, :] @ S["00"] @ Phi.T[:, 2])) / n
+    )
+    return float(f), float(Amp), float(q_s), float(r_s)
+
+
+def compute_kf_nll(y, x, P, KF) -> float:
+    n = len(y) - 1
+    negloglikelihood = 0
+    r_2: float = 0
+    for t in range(1, n + 1):
+        x_, P_ = KF.predict()#KF.predict(x[t], P[t])
+        eps = y[t] - KF.H @ x_
+        r_2 += float(eps @ eps.T)
+        Sigma = KF.H @ P_ @ KF.H.T + KF.R
+        tmp = np.linalg.solve(Sigma, eps)  # Sigma inversion
+        negloglikelihood += 0.5 * (np.log(np.linalg.det(Sigma)) + eps.T @ tmp)
+    return float(negloglikelihood)
+
+
+
+
+
+
     
 class whiteKF:
     """
@@ -125,35 +365,36 @@ class whiteKF:
     https://doi.org/10.1109/TAES.2012.6178086.
     """
     
-    def generate_matsuda_params(fs: float, fc: float):
-        arg = 2.0*np.pi*fc/fs
-        Phi = np.array([[np.cos(arg),-np.sin(arg)],[np.sin(arg),np.cos(arg)]])
-        
-        
-        return Phi, H
-        
+    
 
     def __init__(self, Phi, Q, H, R, init_x = None, init_P = None):
         self.Phi = Phi
         
         
         self.Phi = Phi
-        self.Q = Q
+        
+        if Q is None:
+            self.Q = 1e-3
+        else:
+            self.Q = Q
         self.H = H
-        self.R = R
+        if R is None:
+            self.R = 1e-3
+        else:
+            self.R = R
 
         n_states = Phi.shape[0]
         if (init_x is None) or (init_P is None):
             
-            self.x = np.random.randn(n_states, 1)
+            self.x = np.zeros((n_states,1))#np.random.randn(n_states, 1)
             self.P = np.eye(n_states) * 1e-3
         else:
             self.x = init_x
             self.P = init_P
 
-    def predict(self, state):
-        x_ = self.Phi @ state.x
-        P_ = self.Phi @ state.P @ self.Phi.T + self.Q
+    def predict(self):
+        x_ = self.Phi @ self.x
+        P_ = self.Phi @ self.P @ self.Phi.T + self.Q
         return x_, P_
 
     def update(self, y, x_, P_):
@@ -167,27 +408,70 @@ class whiteKF:
         
         return self.x, self.P
 
-    def update_no_meas(self, state_):
+    def update_no_meas(self, x_, P_):
         """Update step when the measurement is missing"""
-        self.state = state_
-        return state_
+        self.x = x_
+        self.P = P_
+        return self.x, self.P
 
 
 
     def step(self, y):
-         state_ = self.predict(self.state)
-         return self.update(y, state_) if y is not None else self.update_no_meas(state_)
+         x_, P_ = self.predict()
+         return self.update(y, x_, P_) if y is not None else self.update_no_meas(x_, P_)
 
     
+    def apply(self, data):
+        data = np.array(data)
+        Npoints = data.shape[0]
+        filtered = np.zeros(Npoints)
+        analytic = np.zeros((Npoints,2))
+        for i in range(Npoints):
+            filtered[i] =  self.H@self.step(data[i])[0]
+            analytic[i,:] = self.step(data[i])[0][:,0]
+        return filtered, analytic
     
     
+    #def fit_params(self, init_params):
+        
+    
+        
     
 #LOAD REAL DATA
 
 
 import mne
 from matplotlib import pyplot as plt
-def offline_test(): # make path as argument
+
+def offline_test_kf():
+    
+    raw = mne.io.read_raw_bdf('C:/Users/Fedosov/Documents/NeoRec/Records/resting_state_prediciton/NeoRec_1.bdf')
+    
+    data = raw[:,:][0]
+    
+    fs = raw.info['sfreq']
+    
+    central_f = 10.0
+    A = 0.99
+    
+    Phi, H = generate_matsuda_params(fs, central_f, A)
+    
+    
+    
+    
+    
+    kalman = whiteKF(Phi = Phi, H = H, Q = None, R = None, init_x = None, init_P = None)
+    
+    kalman = fit_kf_parameters(data[0,:], kalman, cf = central_f, A = A, fs = fs)
+    
+    
+    _, analytic = kalman.apply(data[0,:])
+    
+    plt.figure()
+    plt.plot(np.linalg.norm(analytic,axis = 1))
+  
+
+def offline_test_cfir(): # make path as argument
     raw = mne.io.read_raw_bdf('C:/Users/Fedosov/Documents/NeoRec/Records/resting_state_prediciton/NeoRec_1.bdf')
     
     data = raw[:,:][0]
@@ -202,15 +486,15 @@ def offline_test(): # make path as argument
     
     plt.figure()
     plt.plot(np.abs(filtered[0]))
-    plt.plot(data[0])
+    #plt.plot(data[0])
+    
+    
+    
+  
     
     
     
     
-    
-    
-    
-    kalman = (Phi, Q, H, R, init_x = None, init_P = None):
 
 
 
